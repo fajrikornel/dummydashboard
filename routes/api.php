ca9d141ba5cbee935eeb98e6fda60883f29a6cbc
Fajri Kornel <fajri.apk@gmail.com>
2021-06-14 19:23:38 +0700

    Added basic views and routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('data',function() {
+    return;
+})->name('api.data');

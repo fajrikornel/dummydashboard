a46934d4de46630392b4bbb53e388438ef268021
Fajri Kornel <fajri.apk@gmail.com>
2021-06-14 20:39:46 +0700

    Added functionality to Data API

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -3,10 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Data;
 
 class DataController extends Controller
 {
     public function data() {
-        return 'Test Data Controller';
+
+        Data::create([
+            'X' => random_int(50,100),
+            'Y' => random_int(65,85),
+        ]);
+        
+        $jsonData = (object) array(
+            'name' => 'dummy data',
+            'data' => Data::latest()->select('X','Y')->take(10)->get());        
+
+        return json_encode($jsonData);
     }
 }
